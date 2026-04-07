92a140da057d04281a60cf81a7693636adfa02cc
mchael brogan <jaypreydoor@gmail.com>
2026-04-07 15:06:28 -0500

    firs

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,11 +4,8 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\mnbro\\wax-monkey\\.cache\\dev-404-page.js")),
   "component---src-pages-about-js": preferDefault(require("C:\\Users\\mnbro\\wax-monkey\\src\\pages\\about.js")),
   "component---src-pages-contact-js": preferDefault(require("C:\\Users\\mnbro\\wax-monkey\\src\\pages\\contact.js")),
-  "component---src-pages-fall-risk-js": preferDefault(require("C:\\Users\\mnbro\\wax-monkey\\src\\pages\\fall-risk.js")),
-  "component---src-pages-index-js": preferDefault(require("C:\\Users\\mnbro\\wax-monkey\\src\\pages\\index.js")),
-  "component---src-pages-pulse-4-pulse-jsx": preferDefault(require("C:\\Users\\mnbro\\wax-monkey\\src\\pages\\Pulse4Pulse.jsx"))
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\mnbro\\wax-monkey\\src\\pages\\index.js"))
 }
 
--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,15 +1,6 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js?export=default" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-fall-risk-js": () => import("./../../../src/pages/fall-risk.js?export=default" /* webpackChunkName: "component---src-pages-fall-risk-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-pulse-4-pulse-jsx": () => import("./../../../src/pages/Pulse4Pulse.jsx?export=default" /* webpackChunkName: "component---src-pages-pulse-4-pulse-jsx" */)
-}
-
-
-
-exports.head = {
-
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
